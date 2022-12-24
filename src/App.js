33d4574b90bfc2c5695b45d28bed709bb9225075
Suprada-2002 <supradatripathy789@gmail.com>
2022-12-24 20:56:52 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.scss";
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+
+//components
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Home />
+        <About />
+        <Skills />
+      </div>
+    </Router>
   );
 }
 
